78983e9ef249ff5b020d2a97a146132b3929ca74
RafaelDeAlmeida <rafael_info_@hotmail.com>
2015-03-03 21:49:42 -0300

    Primeiro commit

---
+++ rpg1/src/com/github/rafaeldealmeida/arquiteturadesoftware/rpg1/Tabuleiro.java
@@ -0,0 +1,9 @@
+package com.github.rafaeldealmeida.arquiteturadesoftware.rpg1;
+
+public class Tabuleiro {
+	private int Matriz[][];
+	
+	public Tabuleiro(int Matriz[][]){
+		this.Matriz = Matriz;
+	}
+}
